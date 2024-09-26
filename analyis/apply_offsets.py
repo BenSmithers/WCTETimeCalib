29f00be74870aefd554f1acbcdde17f68d62faba
Ben Smithers <b.smithers.actual@gmail.com>
2024-09-26 11:03:36 -0700

    faster processing, add timing offset

--- analyis/apply_offsets.py
+++ analyis/apply_offsets.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd 
 import numpy as np 
 import json 
+from tqdm import tqdm 
 
 true = pd.read_csv(os.path.join(os.path.dirname(__file__), "..","data","offsets.csv"))
 
@@ -17,16 +18,22 @@ datafile = os.path.join(
 
 data = np.load(datafile, allow_pickle=True)
 
+#preloading like this saves a TON of time on I/O
+pmt_hits = data["digi_hit_pmt"][:]
+charges = data["digi_hit_charge"][:]
+times = data["digi_hit_time"][:]
+
 shift_times = []
 shift_pmts = []
 shift_charge = []
 
-for runno in range(len(data["digi_hit_pmt"])):
-    these_hit_pmts = data["digi_hit_pmt"][runno]
-    these_charges = data["digi_hit_charge"][runno]
-    these_hit_times = data["digi_hit_time"][runno]
+phase_lock = False 
+frequency = 200 #ns  - associated with a 5/3MHz frequency 
 
-    these_hit_times +=  true["offsets"][these_hit_pmts] 
+for ir, runno in tqdm(enumerate(range(len(pmt_hits)))):
+    these_hit_pmts = pmt_hits[runno]
+    these_charges = charges[runno]
+    these_hit_times = times[runno] + true["offsets"][these_hit_pmts] + ir*frequency
 
     shift_times.append(these_hit_times.tolist())
     shift_pmts.append(these_hit_pmts.tolist())
@@ -43,7 +50,7 @@ outfile = os.path.join(
     os.path.dirname(__file__),
     "..",
     "data",
-    "wcsim_offset.json"
+    "wcsim_offset_lock.json"
 )
 _obj = open(outfile, 'wt')
 json.dump(save_data, _obj)
